2d0beb44b50659888268c09f469b5c9654e17ded
dzhsurf <dzhsurf@gmail.com>
2022-10-06 16:11:53 -0700

    fix deploy build scripts

--- build_all.bat
+++ build_all.bat
@@ -1,29 +1,37 @@
 call conda deactivate
 
 call conda env remove --name poga-py37
-call conda env create -f deploy\\conda-env-py37.yaml
+call conda env create -f deploy\conda-env-py37.yaml
 call conda activate poga-py37
-call python setup.py bdist_wheel
+call poetry env use python
+call poetry build --format=wheel
 call conda deactivate
 
 call conda env remove --name poga-py38
-call conda env create -f deploy\\conda-env-py38.yaml
+call conda env create -f deploy\conda-env-py38.yaml
 call conda activate poga-py38
-call python setup.py bdist_wheel
+call poetry env use python
+call poetry build --format=wheel
 call conda deactivate
 
 call conda env remove --name poga-py39
-call conda env create -f deploy\\conda-env-py39.yaml
+call conda env create -f deploy\conda-env-py39.yaml
 call conda activate poga-py39
-call python setup.py bdist_wheel
+call poetry env use python
+call poetry build --format=wheel
 call conda deactivate
 
 call conda env remove --name poga-py310
-call conda env create -f deploy\\conda-env-py310.yaml
+call conda env create -f deploy\conda-env-py310.yaml
 call conda activate poga-py310
-call python setup.py bdist_wheel
-
-call python setup.py sdist
+call copy /y %CONDA_PREFIX%\Library\bin\ffi.dll %CONDA_PREFIX%\ffi.dll
+call copy /y %CONDA_PREFIX%\Library\bin\ffi-7.dll %CONDA_PREFIX%\ffi-7.dll
+call copy /y %CONDA_PREFIX%\Library\bin\ffi-8.dll %CONDA_PREFIX%\ffi-8.dll
+call copy /y %CONDA_PREFIX%\Library\bin\libssl-1_1.dll %CONDA_PREFIX%\libssl-1_1.dll
+call copy /y %CONDA_PREFIX%\Library\bin\libcrypto-1_1.dll %CONDA_PREFIX%\libcrypto-1_1.dll
+call poetry env use python
+call poetry build --format=wheel
 call conda deactivate
+
 
 
