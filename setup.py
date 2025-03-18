9b78687e7b4102a5dff245d66533ecab61286936
Hiram Deng <72371329+dzhsurf@users.noreply.github.com>
2025-03-18 12:37:49 -0700

    Up 0.1.17

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ import pybind11
 from setuptools import Extension, find_packages, setup
 from setuptools.command.build_ext import build_ext
 
-POGA_VERSION = "0.1.16"
+POGA_VERSION = "0.1.17"
 
 # obtain workdir
 here = os.path.abspath(os.path.dirname(__file__))
